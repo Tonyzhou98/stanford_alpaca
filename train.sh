974735fd713c74f15f585d31fc44aad39f322755
ZYH <31127200+Tonyzhou98@users.noreply.github.com>
2023-12-01 16:33:46 -0500

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 #SBATCH --partition=gpu
 #SBATCH --time=20:00:00
-#SBATCH --ntasks=4
+#SBATCH --mem-per-cpu=20g
 #SBATCH --gres=gpu:4
+#SBATCH --cpus-per-task=16
 
 
-torchrun --master_port=29500 --nproc_per_node=4 train.py \
-    --model_name_or_path /afs/shell.umd.edu/project/aiwie-prj/user/tonyzhou/llama-2-7b-hf/ \
+torchrun --master_port=29600 --nproc_per_node=4 train.py \
+    --model_name_or_path /scratch/zt1/project/aiwie-prj/user/tonyzhou/llama-2-7b-hf/ \
     --data_path ./alpaca_data.json \
     --bf16 True \
-    --output_dir /afs/shell.umd.edu/project/aiwie-prj/user/tonyzhou/checkpoint/ \
+    --output_dir /scratch/zt1/project/aiwie-prj/user/tonyzhou/checkpoint/ \
     --num_train_epochs 1 \
     --per_device_train_batch_size 1 \
     --per_device_eval_batch_size 1 \
@@ -21,5 +22,8 @@ torchrun --master_port=29500 --nproc_per_node=4 train.py \
     --learning_rate 2e-5 \
     --weight_decay 0. \
     --warmup_ratio 0.03 \
-    --deepspeed "./configs/default_offload_opt_param.json" \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --fsdp "full_shard auto_wrap offload" \
+    --fsdp_transformer_layer_cls_to_wrap 'LlamaDecoderLayer' \
     --tf32 True
